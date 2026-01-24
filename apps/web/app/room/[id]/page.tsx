7a7de429a804c2ba767f16752dafa22340e8494d
Forzun <vbhavesh219@gmail.com>
2026-01-25 02:49:36 +0530

    Image fix with FileRender

--- apps/web/app/room/[id]/page.tsx
+++ apps/web/app/room/[id]/page.tsx
@@ -2,9 +2,10 @@
 
 import { Button } from "@workspace/ui/components/button";
 import { Input } from "@workspace/ui/components/input";
-import { Coins, Copy, ImageUp, Send, Trash } from "lucide-react"
+import { count } from "console";
+import { Copy, HandMetal, Send, Trash, X } from "lucide-react"
 import Image from "next/image";
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 interface Message { 
     text: string, 
@@ -14,21 +15,46 @@ interface Message {
 }
 
 export default function Page(){ 
-    const [messages , setMesasges] = useState<Message[]>([
-        {
-            text: "hi", 
-            timestamp: new Date()
-        },
-    ]);
-    const [image ,setImage] = useState("")
+    const [input , setInput] = useState('');
+    const [isActive , setIsActive] = useState(true);
+    const [chatImage , setChatImage] = useState(true);
+    const [messages , setMessage] = useState<Message[]>([]);
 
+    const [image ,setImage] = useState<string | null>(null)
 
-    function handleFileSelection(event:any){ 
-        console.log("here is the event:", event)
-        if (event.target.files && event.target.files[0]) {
-            setImage(URL.createObjectURL(event.target.files[0]));
-            console.log("what happen to image:", image)
-          }
+    function handleImageSubmit(event:any){ 
+        const imageFile = event.target.files[0];
+
+        if(imageFile){ 
+            const render = new FileReader();
+            render.onload = (e) => { 
+                setImage(e.target?.result as string)
+            };
+            render.readAsDataURL(imageFile)
+        }
+
+    }
+
+    function handleMessageSubmit() { 
+        console.log("image length", image)
+        if(input.trim() || image){
+
+            setMessage(prevMessage => [...prevMessage , { 
+                text: input, 
+                timestamp: new Date(),
+                image: image || undefined
+            } ])
+
+            console.log("current message: value", image)
+            setImage(null);
+            setInput('')
+        }
+    }
+
+    function DownKeyHandler(e: KeyboardEvent){
+        if(e.key === "Enter"){
+            handleMessageSubmit();
+        }
     }
 
     return <div className="flex flex-col min-h-screen bg-background">
@@ -67,6 +93,9 @@ export default function Page(){
                                 <div className="text-neutral-400 text-md font-bold pt-2 ">{"<"}</div>
                             <div className="flex flex-col gap-1">
                                 <div className="inline-block px-3 md:px-2 py-2 rounded-md border transition-all duration-200 bg-cover border-b-primary/80 cursor-grab shadow-2xl text-shadow-sm text-shadow-primary/10 ring ring-white/10">
+                                    {message.image && <div className="h-full w-full relative">
+                                        <Image width={500} height={500} alt="preview image" className="max-w-xs rounded ring-2 ring-primary/40  p-2" src={message.image} />
+                                    </div> }
                                     {message.text && <p className="wrap-break-word text-sm leading-normal tracking-tight text-neutral-400">{message.text}</p>}
                                 </div>
                                 <span className="text-xs text-muted-foreground ml-2">
@@ -79,25 +108,27 @@ export default function Page(){
                             </div>
                         </div> 
                     ))
-                ) }
+                )}
             </div>
         </main>
 
 
         <div className="p-5 sticky bottom-0 md:min-w-3xl flex flex-col gap-5 mx-auto">
                 <div className="flex-1 flex flex-col md:px-11 px-4">
-                    {image ? <Image width={500} height={500} alt="preview image" className="max-w-xs p-3" src={image} /> : ""}
+                    {image ? <div className="h-full w-fit relative" >
+                        <Image width={500} height={500} alt="preview image" className="max-w-xs rounded ring-2 ring-primary/40  p-2" src={image} /> 
+                        <button className="w-5 h-5 bg-primary rounded-full p-1 absolute top-3 right-3 flex items-center justify-center text-center"><X /></button>
+                    </div>: ""}
                 </div>
                 
                 <div className="flex items-center gap-3 md:px-7">
                     <span className="md:text-3xl text-2xl shrink-0 text-primary">{"<"}</span>
-                    <Input placeholder="Type message..." className="px-4 py-6 rounded-md" />
+                    <Input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Type message..." className="px-4 py-6 rounded-md" />
                     <div className="flex shrink-0 gap-2">
-                        <Input onChange={handleFileSelection} className="py-6 w-10" type="file" />
-                        <Button className="py-6" ><span className="md:block hidden"><Send /> </span> Send</Button>
+                        <Input onChange={handleImageSubmit} className="py-6 w-10" type="file" />
+                        <Button onKeyDown={() => DownKeyHandler} disabled={input == ''} onClick={handleMessageSubmit} className="py-6" ><span className="md:block hidden"><Send /> </span> Send</Button>
                     </div>
                 </div>
         </div>
-
     </div>
 }
